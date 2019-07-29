23e65657e3c152d1af9b5131b82af25d96e7d613
Zj <519564415@qq.com>
2019-07-29 17:04:48 +0800

    :sparkles:(g-mysql)

--- mysql.conf.js
+++ mysql.conf.js
@@ -1,3 +1,8 @@
+/**
+ * mysql.conf.js: mysql 连接配置
+ *    creator: gainorloss
+ * createTime: 2019-07-29
+ */
 module.exports={
     host:'140.143.88.100',
     user:'root',
--- test.js
+++ test.js
@@ -1,3 +1,8 @@
+/**
+ * test.js: mysql helper 测试
+ *    creator: gainorloss
+ * createTime: 2019-07-29
+ */
 const conf=require('./mysql.conf');
 const mysql=require('./g-mysql')(conf);
 mysql.query('select id,message from test')
